d3fde2fa37553453abb576f55f649af3ba16bf9f
kous500 <so5000ap@gmail.com>
2023-05-11 02:09:40 +0900

    バージョン0.4.0リリース

--- curvebuilding-fabric/src/main/java/me/kous500/curvebuilding/fabric/CurveBuildingFabric.java
+++ curvebuilding-fabric/src/main/java/me/kous500/curvebuilding/fabric/CurveBuildingFabric.java
@@ -13,7 +13,7 @@ import static me.kous500.curvebuilding.fabric.commands.Bc.setBcBuilder;
 import static me.kous500.curvebuilding.fabric.commands.Pos.setPosBuilder;
 
 public class CurveBuildingFabric implements ModInitializer, MainInitializer {
-    public static boolean debugMode = true;
+    public static boolean debugMode = false;
     public static final String MOD_DATA_FOLDER = "config/CurveBuilding";
 
     public static FabricConfig fabricConfig;
