eb1b10c24da00483fcdfd2b193256d8cb817641b
kous <75741517+kous500@users.noreply.github.com>
2025-08-16 00:10:41 +0900

    Revert "1.21.4 Update"

--- curvebuilding-fabric/src/main/java/me/kous500/curvebuilding/fabric/client/render/mixin/GameRendererMixin.java
+++ curvebuilding-fabric/src/main/java/me/kous500/curvebuilding/fabric/client/render/mixin/GameRendererMixin.java
@@ -1,7 +1,6 @@
 package me.kous500.curvebuilding.fabric.client.render.mixin;
 
 import net.minecraft.client.render.*;
-import net.minecraft.client.util.ObjectAllocator;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,20 +10,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static me.kous500.curvebuilding.fabric.client.render.RenderPreview.RenderPosData;
 
 @Mixin(WorldRenderer.class)
-public abstract class GameRendererMixin {
+public abstract class GameRendererMixin{
 
-    @Inject(at = @At("RETURN"), method =
-            "render(" +
-                    "Lnet/minecraft/client/util/ObjectAllocator;" +
-                    "Lnet/minecraft/client/render/RenderTickCounter;" +
-                    "Z" +
-                    "Lnet/minecraft/client/render/Camera;" +
-                    "Lnet/minecraft/client/render/GameRenderer;" +
-                    "Lorg/joml/Matrix4f;" +
-                    "Lorg/joml/Matrix4f;" +
-                    ")V"
-    )
-    private void renderer_postWorldRender(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
-        RenderPosData(matrix4f);
-    }
+        @Inject(at = @At("RETURN"), method =
+                "render(" +
+                "Lnet/minecraft/client/render/RenderTickCounter;" +
+                "Z" +
+                "Lnet/minecraft/client/render/Camera;" +
+                "Lnet/minecraft/client/render/GameRenderer;" +
+                "Lnet/minecraft/client/render/LightmapTextureManager;" +
+                "Lorg/joml/Matrix4f;" +
+                "Lorg/joml/Matrix4f;" +
+                ")V"
+        )
+        private void renderer_postWorldRender(RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
+            RenderPosData(matrix4f);
+        }
 }
